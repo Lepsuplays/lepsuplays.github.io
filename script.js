69a82a75af31472fc3617e8692e8a0a93c06f66e
Lepsuplays <leevikapusta@gmail.com>
2023-11-10 12:44:26 +0200

    Next update and optimisation
    This commit makes website work better on mobile devices

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // Numbers
 var numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+// Functio that toggles element visibility using id
+function toggleVisibility(targetId, hidden, visible) {
+    var target = document.getElementById(targetId);
+    if (target.style.display === visible) {
+        target.style.display = hidden;
+    }
+    else {
+        target.style.display = visible;
+    }
+}
--- sitedata/links.js
+++ sitedata/links.js
@@ -8,12 +8,13 @@
 
 // Data that is showed in sidebar
 var Sidebar = {
-    "Rows": 5,
+    "Rows": 6,
     "Gategories": [
         "mainpage", "mainPage", "false",
         "Used Resourses", "usedResources", "false",
-        "reshade", "reshade", "false",
+        "Games", "games", "false",
         "Tools", "tools", "false",
+        "Fun stuff", "funStuff", "false",
         "My Websites", "mySites", "false"
     ]
 }
@@ -34,10 +35,11 @@ var Sidebar = {
 var Links = {
     "mainPage": [
         {
-            "NumberOfLinks": 18,
+            "NumberOfLinks": 21,
             "Header": "Main",
-            "Description": "Most used links.",
-            "linkItems": ["Docker hub", "https://hub.docker.com/", "Place where you can find docker containers.",
+            "Description": "Theese are links that i use most.",
+            "linkItems": ["Youtube", "https://www.youtube.com/", "Place where you can watch videos.",
+            "Docker hub", "https://hub.docker.com/", "Place where you can find docker containers.",
             "w3schools", "https://www.w3schools.com/html/default.asp", "Place where you can find data for difrent programming languages",
             "FiveM Natives", "https://docs.fivem.net/natives/", "Place where you can find all FiveM natives.",
             "Printables", "https://www.printables.com/", "",
@@ -52,31 +54,51 @@ var Links = {
             "Description": "Theese are links to resourses that i have been using and want to support.",
             "linkItems": ["Stablediffusion", "https://github.com/vladmandic/automatic", "Stabledíffusion build that i have been using.", 
             "Stablediffusion installation guide", "https://rentry.co/GUItard", "Guide that i used when i installed stabblediffusion",
+            "CivitAi","https://civitai.com/models", "Place where you can download difrent stablediffusion models.",
             "EXO Rifter aplication", "https://bit.ly/2Jw3mrG", "Application for EXO Rifter gaming mouse"]
         }
     ],
-    "reshade": [
+    "games": [
         {
-            "NumberOfLinks": 9,
-            "Header": "Reshade",
-            "Description": "Reshade is a program that can show difrent kinds of data or shaders top of games.",
-            "linkItems": ["Reshade main program", "https://reshade.me/", "Main program that has to be downloaded to be able to use following scripts.", 
+            "NumberOfLinks": 30,
+            "Header": "Games",
+            "Description": "Difrent games or mod sites for games.",
+            "linkItems": ["Aternos", "https://aternos.org/", "Website that allows you to host your minecraft server for free.",
+            "Optifine", "https://www.optifine.net/home", "Mod for minecraft that allows difrent additional features and game optimisation.",
+            "Krunker", "https://krunker.io/", "First person shooter game that you can play on your web browser or download it from steam.",
+            "1vs1.lol", "https://1v1.lol/", "Free batleroyale game that you can play in your web browser.",
+            "Farming Simulator mods", "https://www.farming-simulator.com/mods.php", "Place where you can download mods for difrent farming simulator games",
+            "Nexusmods", "https://www.nexusmods.com/", "Place where you can download mods for difrent games.",
+            "Racedepartment", "https://www.racedepartment.com/", "Racedepartment is place where you can download mods for diffrent games.",
+            "Reshade", "https://reshade.me/", "Main program that has to be downloaded to be able to use following scripts.", 
             "Reshade Zoom", "https://github.com/mhgar/ReShade-Magnifier", "Reshade zoom script", 
             "Reshade xhair", "https://github.com/notpeelz/reshade-xhair", "Reshade xhair script"]
         }
     ],
     "tools": [
         {
-            "NumberOfLinks": 21,
+            "NumberOfLinks": 33,
             "Header": "Tools and softwares",
             "Description": "Tools that i have been using and can suggest.",
             "linkItems": ["Visual Studio Code", "https://code.visualstudio.com/", "Code editor that i use.",
             "WinSCP", "https://winscp.net/eng/index.php", "WinSCP is file server client that you can use to move files to your server.",
+            "Discord", "https://discord.com/", "Great message aplication that supports cross platform.",
             "ZoomIt", "https://learn.microsoft.com/en-us/sysinternals/downloads/zoomit", "Program that lets you zoom in on your screen and do markings.",
             "OBS Studio", "https://obsproject.com/", "Easy to use screen recording software.",
             "Blender", "https://www.blender.org/", "3D modeling software where you can make many animations or 3D models.",
             "TinkerCAD", "https://www.tinkercad.com/", "Easy to use 3D modeling software.",
-            "OpenSCAD", "https://openscad.org/", "3D modeling usin SCAD programming language."]
+            "OpenSCAD", "https://openscad.org/", "3D modeling usin SCAD programming language.",
+            "ChatGPT", "https://chat.openai.com/", "Free to use chatbot ai.",
+            "Playground AI", "https://playgroundai.com/", "Free to use ai image generator website.",
+            "Leonardo AI", "https://leonardo.ai/", "Free to use ai image generaator website."]
+        }
+    ],
+    "funStuff": [
+        {
+            "NumberOfLinks": 3,
+            "Header": "Fun stuff",
+            "Description": "All kinds of fun styff that you can play with",
+            "linkItems": ["Minecraft font", "https://github.com/IdreesInc/Monocraft", "Github repository that contains minecraft font and you can download and use it."]
         }
     ],
     "mySites": [
